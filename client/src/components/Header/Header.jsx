6b8be08df903ffe2c05595055d776f57fc94e1f6
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-05-01 16:34:37 +0300

    add component Logo in component Header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import styles from './Header.module.sass';
 import CONSTANTS from '../../constants';
 import { clearUserStore } from '../../store/slices/userSlice';
 import { getUser } from '../../store/slices/userSlice';
+import Logo from '../Logo';
 
 class Header extends React.Component {
   componentDidMount() {
@@ -116,11 +117,12 @@ class Header extends React.Component {
           </div>
         </div>
         <div className={styles.navContainer}>
-          <img
+          {/* <img
             src={`${CONSTANTS.STATIC_IMAGES_PATH}blue-logo.png`}
             className={styles.logo}
             alt="blue_logo"
-          />
+          /> */}
+          <Logo className={styles.logo}/>
           <div className={styles.leftNav}>
             <div className={styles.nav}>
               <ul>
