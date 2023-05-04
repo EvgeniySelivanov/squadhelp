3f49c20d5907271c43edc90ebfc07e3c40175fd7
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-05-04 10:22:27 +0300

    fix userController,chatController

--- server/src/controllers/chatController.js
+++ server/src/controllers/chatController.js
@@ -1,7 +1,6 @@
 const Conversation = require('../models/mongoModels/conversation');
 const Message = require('../models/mongoModels/Message');
 const Catalog = require('../models/mongoModels/Catalog');
-const moment = require('moment');
 const db = require('../models');
 const userQueries = require('./queries/userQueries');
 const controller = require('../socketInit');
