72ecb43ad5d8528eb853638b578a8ba46358f464
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-05-04 13:17:21 +0300

    change method in login

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -1,7 +1,7 @@
 import http from '../interceptor';
 
 export const registerRequest = data => http.post('registration', data);
-export const loginRequest = data => http.get('login', data);
+export const loginRequest = data => http.post('login', data);
 export const getUser = () => http.get('getUser');
 export const updateContest = data => http.patch('updateContest', data);
 export const setNewOffer = data => http.post('setNewOffer', data);
