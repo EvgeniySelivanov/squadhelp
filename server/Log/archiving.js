99e27ec95b815efd2762d3064e3a3ccb085a3761
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-06-02 10:00:38 +0300

    end task 12 and 13

--- server/Log/archiving.js
+++ server/Log/archiving.js
@@ -1,22 +1,26 @@
 const fs = require('fs');
 const archiving = () => {
-  // const logDataFile = fs.readFileSync('./Log/log.json', 'utf-8');
-  // // console.log('>>>>>>>>>>>>>>>>>>', typeof logDataFile);
-  // const obj=JSON.parse(logDataFile);
-  // console.log('obj>>>>>>>>>>>>>>>>>>', obj);
-  // // const dataArchiving=()=>{
-  // //   const logDataFile = fs.readFileSync('./Log/log.json');
-  // //   fs.appendFile('./Log/logArch.json', logDataFile, function (err) {
-  // //     if (err) console.log('error', err);
-  // //   });
-
-  // // };
-  // let time = 0;
-  // setInterval(() => {
-  //   time = new Date;
-  //   if (time.getHours() === 1 && time.getMinutes() === 0 && time.getSeconds() === 0) {
-  //     console.log('archving work!!!!!!!!!!');
-  //   }
-  // }, 1000);
+  let time = 0;
+  setInterval(() => {
+    time = new Date;
+    // console.log(time);
+    if (time.getHours() === 1 && time.getMinutes() === 0 && time.getSeconds() === 0 && fs.existsSync('./Log/log.json') === true) {
+      const readFile = fs.readFileSync('./Log/log.json', 'utf-8');
+      const errTodayObj = JSON.parse(readFile);
+      const archArr = errTodayObj.table;
+      archArr.map((elem) => { return delete elem.stackTrace; });
+      const obj = {
+        table: archArr,
+      };
+      const logData = JSON.stringify(obj, null, 2);
+      fs.writeFile(`./Log/archLog${Date.now()}.json`, logData, function (err) {
+        if (err) console.log('error', err);
+      });
+      fs.unlink('./Log/log.json', function (err) {
+        if (err) throw err;
+        console.log('File deleted!');
+      });
+    }
+  }, 1000);
 };
 module.exports = archiving;
--- server/Log/index.js
+++ server/Log/index.js
@@ -23,7 +23,7 @@ const handlerLog = (error) => {
   }
 
   if (fs.existsSync('./Log/log.json') === true) {
-    const readFile = fs.readFileSync('./Log/log.json', 'utf8', function (err) {
+    const readFile = fs.readFileSync('./Log/log.json', function (err) {
       if (err) { console.log(err); }
     });
     const obj = JSON.parse(readFile);
@@ -33,8 +33,8 @@ const handlerLog = (error) => {
       code: errCode,
       stackTrace: errStack,
     });
-    const json = JSON.stringify(obj, null, 2);
-    fs.writeFile('./Log/log.json', json, function (err) {
+    const logData = JSON.stringify(obj, null, 2);
+    fs.writeFile('./Log/log.json', logData, function (err) {
       if (err) console.log('error', err);
     });
 
