a2284cca4206de767f9d0ecb2569ea12bd3c612d
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-06-07 13:21:00 +0300

    fix task 13

--- server/Log/index.js
+++ server/Log/index.js
@@ -21,8 +21,8 @@ const handlerLog = (error) => {
       if (err) console.log('error', err);
     });
   }
-
-  if (fs.existsSync('./Log/log.json') === true && fs.statSync('./Log/log.json') != 0) {
+  console.log('size log>>> ', fs.statSync('./Log/log.json').size);
+  if (fs.existsSync('./Log/log.json') === true && fs.statSync('./Log/log.json').size != 0) {
     const readFile = fs.readFileSync('./Log/log.json', function (err) {
       if (err) { console.log(err); }
     });
@@ -37,7 +37,22 @@ const handlerLog = (error) => {
     fs.writeFile('./Log/log.json', logData, function (err) {
       if (err) console.log('error', err);
     });
-
+  }
+  if (fs.existsSync('./Log/log.json') === true && fs.statSync('./Log/log.json').size === 0) {
+    const obj = {
+      table: [],
+    };
+    obj.table.push(
+      {
+        message: errMessege,
+        time: timeErr,
+        code: errCode,
+        stackTrace: errStack,
+      });
+    const logData = JSON.stringify(obj, null, 2);
+    fs.writeFile('./Log/log.json', logData, function (err) {
+      if (err) console.log('error', err);
+    });
   }
 };
 
